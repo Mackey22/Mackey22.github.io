fcf63b640b96da1c9ce008dd70932927ab368082
James Mackey <James@dhcp-uris-6353.redrover.cornell.edu>
2015-11-09 15:51:40 -0500

    Adding all source files to the directory

---
+++ scr/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$("#edClick").click(function() {
+		$(window).scrollTo('#education');
+	});
+	$('#wkClick').click(function() {
+		$(window).scrollTo('#work');
+	});
+	$('#projClick').click(function() {
+		$(window).scrollTo('#projects');
+	});
+	$('#intClick').click(function() {
+		$(window).scrollTo('#interests');
+	});
+	$('#resClick').click(function() {
+		$(window).scrollTo('#resume');
+	});
+});
